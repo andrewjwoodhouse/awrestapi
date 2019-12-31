6b92d374a2e19f218f48efe1252d5b3769bedb56
Andrew Woodhouse <andrew_woodhouse@outlook.com>
2019-12-31 16:23:20 +0000

    route handlers code added

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
+import "github.com/gorilla/mux"
+
 func main() {
 
 	// init router
+	r := mux.NewRouter()
+
+	// Route Handlers / Endpoints
+
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBooks).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBooks).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
 }
